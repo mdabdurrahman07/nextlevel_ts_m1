7280f64223a489b125906993c99edd3a743ed9cf
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-04-30 21:59:02 +0600

    Add comments for clarity on nonPrimitive types: object, literal, optional and access modifier "readonly"

--- src/nonPrimitiveType.ts
+++ src/nonPrimitiveType.ts
@@ -1,5 +1,7 @@
 // nonPrimitive types :- array ,object, tuple
 
+//* Array
+
 // string array so string[]
 let bazarList: string[] = ["eggs", "milk", "rice"];
 
@@ -7,9 +9,31 @@ let bazarList: string[] = ["eggs", "milk", "rice"];
 
 let mixedBazarList = ["eggs", 12, "milk", 1, "rice", 10];
 
+//* Tuple
+
 // tuple:- array with a fixed number of elements, where each element has a strictly defined data type at a specific position
 
 // here i can't give3 strings because it's a tuple
 let couple: [string, string] = ["husband", "wife"];
 
 let nameAndRoll: [string, number] = ["jamil", 20];
+
+//* Object
+
+const user: {
+  //* here my organization value is my type, it's called literal value
+  organization: "None";
+
+  //  readonly organization: string; :- readonly is an access modifier
+  firstName: string;
+  //?   here we defined type "OPTIONAL"
+  middleName?: string;
+  lastName: string;
+  isMarried: boolean;
+} = {
+  organization: "None",
+  firstName: "MD Abdur Rahman",
+  middleName: "Nur",
+  lastName: "Jamil",
+  isMarried: false,
+};
